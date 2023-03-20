6383699f5bb5f26fde9dcd43f9809b0bd209352c
Hala <hala-kaissi@outlook.com>
2023-03-20 15:33:18 +0100

    updated output with confidence level

--- classifyInput/MachineLearning_Classification/spacyClassification/classify.py
+++ classifyInput/MachineLearning_Classification/spacyClassification/classify.py
@@ -14,33 +14,34 @@ def pipelines(symptom):
     model = load('model.joblib')
 
     prediction = model.predict([symptom])
-    classification = ""
-    confidence = 0
+    classifications = []
+    confidences = []
 
     with open('machineLearningChapters.json', 'r') as f:
         chapters = list(json.load(f))
 
-    # Create a mapping between chapter IDs and indices in the probability array
-    id_to_index = {}
-    for idx, estimator in enumerate(model.named_steps['classifier'].estimators_):
-        id_to_index[estimator.classes_[0]] = idx
+    for pred in prediction[0]:  # Iterate through multiple predictions
+        classification = ""
+        for chapter in chapters:
+            if chapter['id'] == pred:
+                if chapter['id'] == chapter['chapterId']:
+                    classification = chapter['name']
+                else:
+                    for bigger_chapter in chapters:
+                        if chapter['chapterId'] == bigger_chapter['id']:
+                            classification = bigger_chapter['name'] + " - " + chapter['name']
+                break
+        classifications.append(classification)
 
-    for chapter in chapters:
-        if chapter['id'] == prediction[0][0]:  # Access the index for the predicted class
-            if chapter['id'] == chapter['chapterId']:
-                classification = chapter['name']
-            else:
-                for bigger_chapter in chapters:
-                    if chapter['chapterId'] == bigger_chapter['id']:
-                        classification = bigger_chapter['name'] + " - " + chapter['name']
+    # Obtain the probabilities from the MultiOutputClassifier
+    prediction_proba = model.predict_proba([symptom])
 
-    if prediction[0][0] in id_to_index:
-        # Obtain the probabilities from the LogisticRegression classifier directly
-        probabilities = model.named_steps['classifier'].estimators_[id_to_index[prediction[0][0]]].predict_proba(model.named_steps['vectorizer'].transform([symptom]))[0]
-        predicted_class_index = np.argmax(probabilities)  # Get the index of the predicted class in the probabilities array
-        confidence = probabilities[predicted_class_index]  # Get the probability for the predicted class
+    for i, pred in enumerate(prediction[0]):
+        # Get the probability for the predicted class
+        confidence = prediction_proba[i][0][np.where(model.named_steps['classifier'].estimators_[i].classes_ == pred)[0][0]]
+        confidences.append(confidence)
 
-    return classification, confidence
+    return classifications, confidences
 
 if __name__ == "__main__":
     classification, confidence = pipelines(" ".join(sys.argv[1:]))
